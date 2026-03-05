e9455b25a400e7abf8b244c49ea3f2faf16d1f17
ZICHUANFU-McMiniM4 <fzkuji@ZICHUANFU-McMiniM4A.local>
2026-03-05 21:28:32 +0800

    train: lm_loss_weight 1.0→0.1, let pointer loss dominate

--- jobs/train.sh
+++ jobs/train.sh
@@ -103,7 +103,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
     --lora_target_modules "q_proj,v_proj" \
     --action_head_lr 1e-4 \
     --lora_lr 5e-5 \
-    --lm_loss_weight 1.0 \
+    --lm_loss_weight 0.1 \
     --pointer_loss_weight 1.0 \
     --coord_loss_weight 0.0 \
     --align_crop_mrope false \
